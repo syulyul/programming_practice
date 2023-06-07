81ff3e08aa2b4387abc980a626f0d345de21b985
La Rêveuse <90892290+syulyul@users.noreply.github.com>
2023-06-08 03:40:11 +0900

    [unrated] Title: 더 크게 합치기, Time: 1.23 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        String sa = (String.valueOf(a));
+        String sb = (String.valueOf(b));
+
+        if ((Integer.parseInt(sa + sb)) > (Integer.parseInt(sb + sa))) {
+            answer = (Integer.parseInt(sa + sb));
+        } else if ((Integer.parseInt(sa + sb)) == (Integer.parseInt(sb + sa))) {
+            answer = (Integer.parseInt(sa + sb));
+        } else {
+            answer = (Integer.parseInt(sb + sa));
+        }
+        
+        return answer;
+    }
+}
