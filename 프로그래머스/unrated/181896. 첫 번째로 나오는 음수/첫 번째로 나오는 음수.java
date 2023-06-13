899a70f5664a153b7a43facafa81703495d15487
La Rêveuse <90892290+syulyul@users.noreply.github.com>
2023-06-14 01:22:52 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -1,21 +1,11 @@
 class Solution {
     public int solution(int[] num_list) {
-        int answer = 0;
-        
+       
         for (int i = 0; i < num_list.length; i++) {
-          if (num_list[i] >= 0) {
-            continue;
-          } else {
-            answer = i;
-            return answer;
+          if (num_list[i] < 0) {
+            return i;
           }
         }
-        if (answer == 0) {
-          answer = -1;
-        }
-
-
-        
-        return answer;
+        return -1;
     }
 }
