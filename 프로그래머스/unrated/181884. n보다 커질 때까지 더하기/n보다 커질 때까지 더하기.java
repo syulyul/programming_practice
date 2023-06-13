2df22748dd5152b4fb0ccdb4b835b02a30585c7f
La Rêveuse <90892290+syulyul@users.noreply.github.com>
2023-06-13 22:59:26 +0900

    [unrated] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -3,12 +3,11 @@ class Solution {
         int answer = 0;
         
         for (int i = 0; i < numbers.length; i++) {
-            
             if (answer <= n) {
                 answer += numbers[i];
             }
         }
-        return answer;
         
+        return answer;
     }
 }
