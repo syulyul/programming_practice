c44e25c5b1481e98a35fabef2232132bb7563a65
La Rêveuse <90892290+syulyul@users.noreply.github.com>
2023-06-18 17:28:55 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.05 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,16 @@
+import java.lang.Math;
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+
+    if ((a & 1) == 1 && (b & 1) == 1) {
+      answer += (a * a + b * b);
+    } else if ((a & 1) == 1 || (b & 1) == 1) {
+      answer += 2 * (a + b);
+    } else {
+      answer += Math.abs(a - b);
+    }
+
+    return answer;
+    }
+}
