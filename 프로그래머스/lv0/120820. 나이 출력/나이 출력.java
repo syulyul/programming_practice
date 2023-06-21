7c04da00be3172de4bf64b209f77c941bd68c25d
Rêveuse <90892290+syulyul@users.noreply.github.com>
2023-06-21 23:24:25 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
@@ -2,7 +2,9 @@ class Solution {
     public int solution(int age) {
         int answer = 0;
         
-        answer = 2022 - age + 1;
+        if (age > 0 && age <= 120) {
+            answer = 2022 - age + 1;
+        }
         
         return answer;
     }
