9a0ae5fcac60269637da2577076785496ccc0810
Rêveuse <90892290+syulyul@users.noreply.github.com>
2023-07-01 15:02:51 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 2;
+        double num = Math.sqrt((double) n);
+        
+        if ((int)num * (int)num  == n) {
+            answer = 1;
+        }
+        
+        return answer;
+    }
+}
