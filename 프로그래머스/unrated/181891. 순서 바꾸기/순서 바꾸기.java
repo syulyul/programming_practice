57d23de7b9212b465de43652c8ab569434bc7c5f
Rêveuse <90892290+syulyul@users.noreply.github.com>
2023-07-04 22:49:11 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.05 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = n; i < num_list.length; i++) {
+            list.add(num_list[i]);
+        }
+        for (int i = 0; i < n; i++) {
+            list.add(num_list[i]);
+        }
+        
+        answer = new int[num_list.length];
+        
+        for (int i = 0; i < num_list.length; i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
