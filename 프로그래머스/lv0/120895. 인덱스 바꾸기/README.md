3cddcbbe8cc260f82d53e5c3b7c4f057824a3789
Rêveuse <90892290+syulyul@users.noreply.github.com>
2023-07-05 23:42:36 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 1.39 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/README.md
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 인덱스 바꾸기 - 120895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120895) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120895?language=java) 
 
 ### 성능 요약
 
-메모리: 77.8 MB, 시간: 1.52 ms
+메모리: 76.1 MB, 시간: 1.39 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,18 +1,15 @@
 class Solution {
     public String solution(String my_string, int num1, int num2) {
         String answer = "";
-        String[] str = new String[my_string.length()];
+        String[] split = my_string.split("");
         String temp = "";
         
-        for (int i = 0; i < my_string.length(); i++) {
-            str[i] = String.valueOf(my_string.charAt(i));
-        }
-        temp = str[num2];
-        str[num2] = str[num1];
-        str[num1] = temp;
+        temp = split[num2];
+        split[num2] = split[num1];
+        split[num1] = temp;
         
-        for (int i = 0; i < str.length; i++) {
-            answer += str[i];
+        for (String str : split) {
+            answer += str;
         }
         
         return answer;
