e3a6868cdafd7817c02d30259ec81fa8d0890cbb
Rêveuse <90892290+syulyul@users.noreply.github.com>
2023-07-09 15:58:55 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.java
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int order) {
         int answer = 0;
-        String order_s = String.valueOf(order);
         
-        for (int i = 0; i < order_s.length(); i++) {
-            if (order_s.charAt(i) == '3' || order_s.charAt(i) == '6' || order_s.charAt(i) == '9') {
+        while (order >= 1) {
+            if (order % 10 == 3 || order % 10 == 6 || order % 10 == 9) {
                 answer++;
             }
+            order /= 10;
         }
         
         return answer;
--- 프로그래머스/lv0/120891. 369게임/README.md
+++ 프로그래머스/lv0/120891. 369게임/README.md
@@ -1,10 +1,10 @@
 # [level 0] 369게임 - 120891 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120891) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120891?language=java) 
 
 ### 성능 요약
 
-메모리: 75.8 MB, 시간: 0.04 ms
+메모리: 74 MB, 시간: 0.02 ms
 
 ### 구분
 
