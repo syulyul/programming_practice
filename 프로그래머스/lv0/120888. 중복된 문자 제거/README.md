ec04a5572d471de7aa9eccae9408ac5f02c5605f
Rêveuse <90892290+syulyul@users.noreply.github.com>
2023-07-19 00:52:25 +0900

    [level 0] Title: 중복된 문자 제거, Time: 11.38 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv0/120888. 중복된 문자 제거/README.md
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/README.md
@@ -1,10 +1,10 @@
 # [level 0] 중복된 문자 제거 - 120888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120888) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120888?language=java) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 1.87 ms
+메모리: 79 MB, 시간: 11.38 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -1,27 +1,11 @@
-import java.util.*;
-
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        String[] arr = my_string.split("");
-        ArrayList<String> list = new ArrayList<>();
         
-        for (String s : arr) {
-            list.add(s);
-        }
-        
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(i).equals(list.get(j))) {
-                    System.out.println(j);
-                    list.remove(j);
-                    j--;
-                }
+        for (int i = 0; i < my_string.length(); i++) {
+            if (my_string.indexOf(my_string.charAt(i)) == i) {
+                answer += my_string.charAt(i);
             }
-        }
-        
-        for (int i = 0; i < list.size(); i++) {
-            answer += list.get(i);
         }
         
         return answer;
