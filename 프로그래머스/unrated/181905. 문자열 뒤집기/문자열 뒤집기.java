7c2debc3dfdccce3a61697a0ebbdbdd9ea4e7bc2
Rêveuse <90892290+syulyul@users.noreply.github.com>
2023-07-30 18:54:25 +0900

    [unrated] Title: 문자열 뒤집기, Time: 11.43 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -3,21 +3,16 @@ import java.util.*;
 class Solution {
     public String solution(String my_string, int s, int e) {
         String answer = "";
-        ArrayList<String> rev = new ArrayList<>();
-        int start = my_string.length() - 1 - e;
-        
-        for (int i = my_string.length() - 1; i >= 0; i--) {
-            rev.add(String.valueOf(my_string.charAt(i)));
+        char[] spell = my_string.toCharArray();
+
+        for (int i = s; i < e; i++, e--) {
+            char tmp = spell[i];
+            spell[i] = spell[e];
+            spell[e] = tmp;
         }
         
-        for (int i = 0; i < my_string.length(); i++) {
-            if (i < s) {
-                answer += my_string.charAt(i);
-            } else if (i >= s && i <= e) {
-                answer += rev.get(start++);
-            } else {
-                answer += my_string.charAt(i);
-            }
+        for (char c : spell) {
+            answer += c;
         }
         
         return answer;
