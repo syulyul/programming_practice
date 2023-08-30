1a56d22c4dd330a9abedd0527ff0a3adf1ca4814
Rêveuse <90892290+syulyul@users.noreply.github.com>
2023-08-30 13:54:04 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        String str_x = String.valueOf(x);
+        int num = 0;
+        
+        for (int i = 0; i < str_x.length(); i++) {
+            num += (str_x.charAt(i) - '0');
+        }
+        
+        if (x % num != 0) answer = false;
+        else answer = true;
+        
+        return answer;
+    }
+}
