e0b51faaf53b1e86555f90effddc41dcfb0f5e5c
Rêveuse <90892290+syulyul@users.noreply.github.com>
2023-09-07 19:51:50 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 2.78 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,11 +1,16 @@
 class Solution {
     public String solution(String my_string, int[][] queries) {
         StringBuilder sb = new StringBuilder(my_string);
-        StringBuilder rev;
         
         for (int i = 0; i < queries.length; i++) {
-            rev = new StringBuilder(sb.substring(queries[i][0], queries[i][1] + 1)).reverse();
-            sb.replace(queries[i][0], queries[i][1] + 1, rev.toString());
+            int start = queries[i][0];
+            int end = queries[i][1];
+            
+            // 뒤집을 부분 문자열을 추출한 후 뒤집기
+            StringBuilder rev = new StringBuilder(sb.substring(start, end + 1)).reverse();
+            
+            // 원본 문자열에서 뒤집힌 부분을 교체하기
+            sb.replace(start, end + 1, rev.toString());
         }
         
         return sb.toString();
