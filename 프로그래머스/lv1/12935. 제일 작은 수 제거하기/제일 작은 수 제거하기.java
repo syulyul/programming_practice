87026bb5b20dac460e87574340be3b3b07569957
Rêveuse <90892290+syulyul@users.noreply.github.com>
2023-09-11 23:50:26 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.69 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        if (arr.length <= 1) {
+            // 배열의 길이가 1 이하인 경우, -1을 포함한 배열 반환
+            return new int[]{-1};
+        } else {
+            // 배열을 복사하여 정렬
+            int[] sortedArr = Arrays.copyOf(arr, arr.length);
+            Arrays.sort(sortedArr);
+
+            // 가장 작은 수 찾기
+            int min = sortedArr[0];
+
+            // 가장 작은 수를 제외한 새로운 배열 생성
+            int[] ret = new int[arr.length - 1];
+            int idx = 0;
+            for (int value : arr) {
+                if (value != min) {
+                    ret[idx++] = value;
+                }
+            }
+
+            return ret;
+        }
+    }
+}
