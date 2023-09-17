196799c93f59b12f8c647519e7f33c3de480d986
Rêveuse <90892290+syulyul@users.noreply.github.com>
2023-09-17 15:27:11 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        
+        for (int i = 0; i < db.length; i++) {
+            if (id_pw[0].equals(db[i][0])) {
+                if (!id_pw[1].equals(db[i][1])) {
+                    answer = "wrong pw";
+                } else {
+                    answer = "login";
+                }
+                break;
+            } else {
+                answer = "fail";
+            }
+        }
+        
+        return answer;
+    }
+}
