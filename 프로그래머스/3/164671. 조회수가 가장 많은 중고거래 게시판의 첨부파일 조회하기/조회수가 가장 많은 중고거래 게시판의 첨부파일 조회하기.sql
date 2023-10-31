cb9216612d9c78149f20b63310269141c3754cda
Prometedor <90892290+syulyul@users.noreply.github.com>
2023-10-31 10:56:42 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
-SELECT CONCAT('/home/grep/src/', a.BOARD_ID, '/', b.FILE_ID, b.FILE_NAME, b.FILE_EXT) AS FILE_PATH
+SELECT CONCAT('/home/grep/src/', b.BOARD_ID, '/', b.FILE_ID, b.FILE_NAME, b.FILE_EXT) AS FILE_PATH
 FROM USED_GOODS_BOARD a JOIN
     USED_GOODS_FILE b
     ON a.BOARD_ID = b.BOARD_ID
