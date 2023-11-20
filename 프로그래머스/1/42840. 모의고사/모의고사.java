d5d981e6be96fb250de4cc65272ecc4912d7b9f3
Prometedor <90892290+syulyul@users.noreply.github.com>
2023-11-20 17:39:47 +0900

    [level 1] Title: 모의고사, Time: 4.77 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,60 +1,43 @@
-import java.util.*;
+import java.util.stream.IntStream;
 
 class Solution {
+    
+    private static final int[][] RULES = {
+        { 1, 2, 3, 4, 5 },
+        { 2, 1, 2, 3, 2, 4, 2, 5 },
+        { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 }
+    };
+    
+    private int getPicked(int person, int problem) {
+        int[] rule = RULES[person];
+        int idx = problem % rule.length;
+        return rule[idx];
+    }
+    
     public int[] solution(int[] answers) {
-        int[] answer = {};
-        int[] giveupMath1 = { 1, 2, 3, 4, 5 };
-        int[] giveupMath2 = { 2, 1, 2, 3, 2, 4, 2, 5 };
-        int[] giveupMath3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
-        int[] score = new int[3];
-        int cnt = 0;
+        int[] corrects = new int[3];
         int max = 0;
-        ArrayList<Integer> al = new ArrayList<>();
-        int rank = 1;
         
-        for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == giveupMath1[cnt++]) {
-                score[0]++;
-            }
-            if (cnt == giveupMath1.length) cnt = 0;
-        }
-        
-        cnt = 0;
-        
-        for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == giveupMath2[cnt++]) {
-                score[1]++;
-            }
-            if (cnt == giveupMath2.length) cnt = 0;
-        }
-        
-        cnt = 0;
-        
-        for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == giveupMath3[cnt++]) {
-                score[2]++;
-            }
-            if (cnt == giveupMath3.length) cnt = 0;
-        }
-        
-        for (int i = 0; i < score.length; i++) {
-            if (score[i] > max) {
-                max = score[i];
+        for (int problem = 0; problem < answers.length; problem++) {
+            int answer = answers[problem];
+            
+            for (int person = 0; person < RULES.length; person++) {
+                int picked = getPicked(person, problem);
+                if (answer == picked) {
+                    corrects[person]++;
+                    
+                    if (corrects[person] > max) {
+                        max = corrects[person];
+                    }
+                }
             }
         }
         
-        for (int i = 0; i < score.length; i++) {
-            if (score[i] == max) {
-                al.add(i + 1);
-            }
-        }
+        final int maxCorrects = max;
+        return IntStream.range(0, 3)
+                .filter(i -> corrects[i] == maxCorrects)
+                .map(i -> i + 1)
+                .toArray();
         
-        answer = new int[al.size()];
-        
-        for (int i = 0; i < al.size(); i++) {
-            answer[i] = al.get(i);
-        }
-        
-        return answer;
     }
 }
