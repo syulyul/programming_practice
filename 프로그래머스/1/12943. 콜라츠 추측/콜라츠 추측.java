591e018503b71ec17196174a8cb875595aefa3e7
Prometedor <90892290+syulyul@users.noreply.github.com>
2023-11-29 16:30:10 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -6,11 +6,10 @@ class Solution {
         if (n == 1) return 0;
         
         while (n != 1 && cnt != 500) {
-            if ((n & 1) == 0) {
+            if (n % 2 == 0) {
                 n /= 2;
-            } else if ((n & 1) == 1) {
-                n *= 3;
-                n += 1;
+            } else if (n % 2 != 0) {
+                n = n * 3 + 1;
             }
             cnt++;
         }
