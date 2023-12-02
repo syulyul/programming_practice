9134097c5188c0beecbf63ce4b7aa728efa3c84d
Prometedor <90892290+syulyul@users.noreply.github.com>
2023-12-02 18:02:01 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.20 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -2,14 +2,10 @@ class Solution {
     public boolean solution(String s) {
         boolean answer = true;
         
-        if (s.length() == 4 || s.length() == 6) {
-            for (int i = 0; i < s.length(); i++) {
-                if ((s.charAt(i) >= 'a' && s.charAt(i) <= 'z') || (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z')) {
-                    return false;
-                }
-            }   
-        } else {
-            return false;
+        if (s.length() != 4 && s.length() != 6) return false;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (!s.matches("^[0-9]*$")) return false;
         }
         
         return answer;
