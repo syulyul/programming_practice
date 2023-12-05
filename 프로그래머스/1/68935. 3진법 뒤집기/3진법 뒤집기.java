9c686de8e2812f672dbf4549a1077ac8869330c8
Prometedor <90892290+syulyul@users.noreply.github.com>
2023-12-05 15:20:51 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        String n3 = "";
+        int[] rev = {};
+        int idx = 0;
+        
+        n3 = Integer.toString(n, 3);
+        
+        int answer = n3.charAt(0) - '0';
+        for (int i = 1; i < n3.length(); i++) {
+            int cnt = 1;
+            for (int j = 0; j < i; j++) cnt *= 3;
+            answer += cnt * (n3.charAt(i) - '0');
+        }
+        
+        return answer;
+    }
+}
