f30bb0ab817d9e572eb562eb46106040334f7594
Prometedor <90892290+syulyul@users.noreply.github.com>
2023-12-10 13:10:19 +0900

    [level 1] Title: 최소직사각형, Time: 140.81 ms, Memory: 93.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int max0 = 1;
+        int max1 = 1;
+        
+        for (int i = 0; i < sizes.length; i++) {
+            if (sizes[i][0] >= sizes[i][1]) {
+                int tmp = sizes[i][1];
+                sizes[i][1] = sizes[i][0];
+                sizes[i][0] = tmp;
+            }
+            
+            for (int j = 0; j < sizes.length - 1; j++) {
+                if (sizes[i][0] > max0) max0 = sizes[i][0];
+                if (sizes[i][1] > max1) max1 = sizes[i][1];
+            }
+            answer = max0 * max1;
+        }
+        
+        return answer;
+    }
+}
