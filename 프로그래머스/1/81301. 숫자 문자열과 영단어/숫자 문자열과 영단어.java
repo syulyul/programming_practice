839dda57bc85aa09abf3813d22171b8235349949
Prometedor <90892290+syulyul@users.noreply.github.com>
2023-12-12 17:51:10 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.39 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,36 @@
+import java.util.Map;
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String s) {
+        Map<String, Integer> wordToNum = new HashMap<>() {{
+            put("zero", 0);
+            put("one", 1);
+            put("two", 2);
+            put("three", 3);
+            put("four", 4);
+            put("five", 5);
+            put("six", 6);
+            put("seven", 7);
+            put("eight", 8);
+            put("nine", 9);
+        }};
+
+        StringBuilder numStr = new StringBuilder();
+        StringBuilder currWord = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                numStr.append(c);
+            } else {
+                currWord.append(c);
+                if (wordToNum.containsKey(currWord.toString())) {
+                    numStr.append(wordToNum.get(currWord.toString()));
+                    currWord.setLength(0);
+                }
+            }
+        }
+
+        return Integer.parseInt(numStr.toString());
+    }
+}
