7c118f20a4a8572f6baaedec0f9dbf274f0ca29e
Prometedor <90892290+syulyul@users.noreply.github.com>
2023-12-15 15:03:25 +0900

    [level 1] Title: K번째수, Time: 0.48 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,10 +3,9 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        int[] arr = {};
         
         for (int i = 0; i < commands.length; i++) {
-            arr = new int[commands[i][1] - commands[i][0] + 1];
+            int[] arr = new int[commands[i][1] - commands[i][0] + 1];
             arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
             Arrays.sort(arr);
             answer[i] = arr[commands[i][2] - 1];
