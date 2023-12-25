c9c88129d932b47a67320c1e0befb7251830c6d9
Prometedor <90892290+syulyul@users.noreply.github.com>
2023-12-25 15:39:12 +0900

    [Silver V] Title: 영화감독 숌, Time: 300 ms, Memory: 86700 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+  static int titleN = 0;
+  static String title = "";
+  public static void main(String[] args) throws Exception {
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(br.readLine());
+
+    int cnt = 0;
+    int num = 666;
+
+    while (true) {
+      if (String.valueOf(num).contains("666")) {
+          cnt++;
+      }
+
+      if (cnt == n) {
+        bw.write(String.valueOf(num));
+          break;
+      }
+      num++;
+    }
+    bw.newLine();
+    bw.flush();
+  }
+}
