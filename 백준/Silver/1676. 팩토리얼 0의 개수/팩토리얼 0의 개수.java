eadad0d082ea45ee709cd3385e6bb6678a332649
Prometedor <90892290+syulyul@users.noreply.github.com>
2024-01-01 14:48:34 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -18,8 +18,6 @@ public class Main {
 private static int countZerosInFactorial(int n) {
     int cnt = 0;
 
-    // 2와 5의 갯수 중 더 작은 것이 0의 갯수를 결정한다.
-    // 5의 갯수가 더 적으므로 5의 갯수를 세기
     for (int i = 5; i <= n; i *= 5) {
         cnt += n / i;
     }
