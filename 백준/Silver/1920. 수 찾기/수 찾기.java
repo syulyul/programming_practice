752458c9470624ee36ca665c7f122e06429e33a9
Prometedor <90892290+syulyul@users.noreply.github.com>
2024-01-03 16:37:23 +0900

    [Silver IV] Title: 수 찾기, Time: 596 ms, Memory: 53568 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,30 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
 
-    int N = Integer.parseInt(br.readLine());
-    HashSet<Integer> arr1 = new HashSet<>();
-    StringTokenizer st1 = new StringTokenizer(br.readLine());
-    for (int i = 0; i < N; i++) {
-      arr1.add(Integer.parseInt(st1.nextToken()));
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+          int num = Integer.parseInt(st.nextToken());
+          bw.write(set.contains(num) ? String.valueOf(1) : String.valueOf(0));
+          bw.newLine();
+        }
+        bw.flush();
+        bw.close();
     }
-    int M = Integer.parseInt(br.readLine());
-    StringTokenizer st2 = new StringTokenizer(br.readLine());
-    for (int i = 0; i < M; i++) {
-      if (arr1.contains(Integer.parseInt(st2.nextToken()))) {
-        bw.write(String.valueOf(1));
-        bw.newLine();
-      } else {
-        bw.write(String.valueOf(0));
-        bw.newLine();
-      }
-    }
-
-    bw.flush();
-    bw.close();
-  }
 }
