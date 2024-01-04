70314e4649f603fa0b139db9b03074532c07eedc
Prometedor <90892290+syulyul@users.noreply.github.com>
2024-01-04 13:06:07 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.38 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -1,26 +1,25 @@
-import java.util.PriorityQueue;
+import java.util.*;
 
-public class Solution {
+class Solution {
     public int[] solution(int k, int[] score) {
-        int[] result = new int[score.length];
+        int[] answer = new int[score.length];
+
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int[] prevRank = new int[score.length];
 
-        for (int i = 0; i < score.length; i++) {
-            pq.add(score[i]);
-            prevRank[score[i]] = i + 1;
-
-            if (i < k - 1) {
-                result[i] = pq.peek();
-            } else {
-                while (prevRank[pq.peek()] <= i - k + 1) {
-                    pq.poll();
-                }
-
-                result[i] = pq.peek();
+        for(int i = 0; i < score.length; i++) {
+            pq.add(score[i]); // 현재 가수의 점수를 우선순위 큐에 추가
+            
+            // 우선순위 큐의 크기가 k를 초과하면, 큐에서 가장 작은 값을 제거 
+            // 이렇게 함으로써 명예의 전당에는 상위 k개의 점수만 유지됨
+            if (pq.size() > k) {
+                pq.poll();
             }
+            
+            // 현재까지의 명예의 전당에 있는 최하위 점수를 배열에 저장 
+            // PriorityQueue의 peek 메서드는 큐에서 가장 작은 값을 반환
+            answer[i] = pq.peek();
         }
 
-        return result;
+        return answer;
     }
 }
