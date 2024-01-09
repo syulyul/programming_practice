111a657c54fd815c1b2e40cb0104305227b12f9f
Prometedor <90892290+syulyul@users.noreply.github.com>
2024-01-09 18:52:09 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -2,48 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    // 입력 받기
     StringTokenizer st = new StringTokenizer(br.readLine());
     int N = Integer.parseInt(st.nextToken());
     int M = Integer.parseInt(st.nextToken());
 
-    String[] str = br.readLine().split("\\s+");
-    int[] num = new int[N];
+    int[] cards = new int[N];
+
+    st = new StringTokenizer(br.readLine());
     for (int i = 0; i < N; i++) {
-      num[i] = Integer.parseInt(str[i]);
+      cards[i] = Integer.parseInt(st.nextToken());
     }
 
-    HashMap<Integer, Integer> hm = new HashMap<>();
-    
-    for (int i = 0; i < N; i++) {
-      hm.put(i, M - num[i]);
-    }
-    
-    List<Map.Entry<Integer, Integer>> el = new LinkedList<>(hm.entrySet());
-    el.sort(Map.Entry.comparingByValue());
-    
-    
     int maxSum = 0;
-    for (int i = 0; i < el.size() - 2; i++) {
+
+    // 가능한 모든 카드 조합의 합 계산
+    for (int i = 0; i < N - 2; i++) {
       for (int j = i + 1; j < N - 1; j++) {
         for (int k = j + 1; k < N; k++) {
-          int sum = 0;
-          Map.Entry<Integer, Integer> entry = el.get(i);
-          sum += num[entry.getKey()];
-          entry = el.get(j);
-          sum += num[entry.getKey()];
-          entry = el.get(k);
-          sum += num[entry.getKey()];
+          int sum = cards[i] + cards[j] + cards[k];
 
-          if (sum <= M && sum >= maxSum) {
-            maxSum = sum;
+          // 주어진 M을 넘지 않으면서 최대한 가까운 합 갱신
+          if (sum <= M && sum > maxSum) {
+              maxSum = sum;
           }
         }
       }
     }
-    
+
+    // 결과 출력
     bw.write(String.valueOf(maxSum));
     bw.newLine();
     bw.flush();
