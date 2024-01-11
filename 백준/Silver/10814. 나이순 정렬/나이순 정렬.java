91f25f9e1f9f8f20303e77dc43fa18eb3e447f21
Prometedor <90892290+syulyul@users.noreply.github.com>
2024-01-11 17:43:14 +0900

    [Silver V] Title: 나이순 정렬, Time: 716 ms, Memory: 48728 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        String[][] arr = new String[N][2];
+        
+        for (int i = 0; i < N; i++) {
+          StringTokenizer st = new StringTokenizer(br.readLine());
+          arr[i][0] = st.nextToken();
+          arr[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+          return Integer.compare(Integer.parseInt(a[0]), Integer.parseInt(b[0]));
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+          sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+        }
+        bw.write(sb.toString());
+        
+        bw.close();
+    }
+}
