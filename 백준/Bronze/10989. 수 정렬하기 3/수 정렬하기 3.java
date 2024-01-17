671eb53d9d729a15fab56ad19989be1a87e70dbb
Prometedor <90892290+syulyul@users.noreply.github.com>
2024-01-17 17:38:43 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1636 ms, Memory: 337320 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -7,18 +7,22 @@ public class Main {
 
   public static void main(String[] args) throws IOException {
 
+    int[] arr = new int[10001];
+
     int N = Integer.parseInt(br.readLine());
-    int[] num = new int[N];
 
     for (int i = 0; i < N; i++) {
-      num[i] = Integer.parseInt(br.readLine());
+      arr[Integer.parseInt(br.readLine())]++;
     }
-
-    Arrays.sort(num);
+    br.close();
 
     StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < N; i++) {
-      sb.append(num[i]).append("\n");
+
+    for (int i = 0; i < arr.length; i++) {
+      while(arr[i]>0){
+        sb.append(i).append('\n');
+        arr[i]--;
+      }
     }
     bw.write(sb.toString());
     bw.close();
