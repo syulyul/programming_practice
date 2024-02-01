ea4b5ddf52f7e9c2268b4f3230941d6db41d3b06
Prometedor <90892290+syulyul@users.noreply.github.com>
2024-02-01 16:42:14 +0900

    [Silver III] Title: 1로 만들기, Time: 160 ms, Memory: 18464 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+  
+  public static void main(String[] args) throws IOException {
+    int N = Integer.parseInt(br.readLine());
+    br.close();
+    
+    int[] dp = new int[N + 1];
+    dp[1] = 0; // 1은 이미 1이므로 연산 횟수가 0입니다.
+
+    // 연산 횟수 갱신
+    for (int i = 2; i <= N; i++) {
+      dp[i] = dp[i - 1] + 1; // 1을 빼는 경우
+      if (i % 2 == 0)
+        dp[i] = Math.min(dp[i], dp[i / 2] + 1); // 2로 나누는 경우
+      if (i % 3 == 0)
+        dp[i] = Math.min(dp[i], dp[i / 3] + 1); // 3으로 나누는 경우
+    }
+    bw.write(dp[N] + "\n");
+    bw.close();
+  }
+}
