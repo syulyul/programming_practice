8372500b2993da67e528a2356f140825947aced9
Prometedor <90892290+syulyul@users.noreply.github.com>
2024-02-16 14:45:29 +0900

    [Silver I] Title: 절댓값 힙, Time: 352 ms, Memory: 27504 KB -BaekjoonHub

--- 백준/Silver I/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver I/11286. 절댓값 힙/절댓값 힙.java
@@ -8,7 +8,9 @@ public class Main {
   public static void main(String[] args) throws Exception {
     int N = Integer.parseInt(br.readLine());
 
-    PriorityQueue<Integer> pq = new PriorityQueue<>(new AbsComparator());
+    PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> 
+      Math.abs(a) == Math.abs(b) ? a - b : Math.abs(a) - Math.abs(b)
+    );
 
     for (int i = 0; i < N; i++) {
       int x = Integer.parseInt(br.readLine());
@@ -22,23 +24,9 @@ public class Main {
         pq.offer(x);
       }
     }
-
+    
     bw.flush();
     br.close();
     bw.close();
-  }
-  
-  // Comparator 구현
-  static class AbsComparator implements Comparator<Integer> {
-    @Override
-    public int compare(Integer a, Integer b) {
-      int absA = Math.abs(a);
-      int absB = Math.abs(b);
-      if (absA == absB) {
-        return a - b;
-      } else {
-        return absA - absB;
-      }
-    }
   }
 }
