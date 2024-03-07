beb9769208e511c4f08172565d115e5e72e6ada7
Prometedor <90892290+syulyul@users.noreply.github.com>
2024-03-07 22:01:51 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 116 ms, Memory: 14024 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+  private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+  public static void main(String[] args) throws Exception {
+    bw.write("       _.-;;-._\n");
+    bw.write("\'-..-\'|   ||   |\n");
+    bw.write("\'-..-\'|_.-;;-._|\n");
+    bw.write("\'-..-\'|   ||   |\n");
+    bw.write("\'-..-\'|_.-''-._|\n");
+    bw.flush();
+    br.close();
+    bw.close();
+  }
+}
