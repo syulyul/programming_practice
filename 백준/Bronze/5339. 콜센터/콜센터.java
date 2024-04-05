445e2bf1de18ff07db3e5430944fd99b5f46a27e
Prometedor <90892290+syulyul@users.noreply.github.com>
2024-04-05 17:56:13 +0900

    [Bronze V] Title: 콜센터, Time: 124 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+        bw.write("     /~\\\n");
+        bw.write("    ( oo|\n");
+        bw.write("    _\\=/_\n");
+        bw.write("   /  _  \\\n");
+        bw.write("  //|/.\\|\\\\\n");
+        bw.write(" ||  \\ /  ||\n");
+        bw.write("============\n");
+        bw.write("|          |\n");
+        bw.write("|          |\n");
+        bw.write("|          |\n");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
