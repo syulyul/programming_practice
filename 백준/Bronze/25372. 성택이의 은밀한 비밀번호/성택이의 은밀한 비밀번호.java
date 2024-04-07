4a4999e9f6282a800ae5edb1d72f68f1328b0b9c
Prometedor <90892290+syulyul@users.noreply.github.com>
2024-04-07 22:24:20 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 136 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception {
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            if (str.length() >= 6 && str.length() <= 9)
+                bw.write("yes\n");
+            else
+                bw.write("no\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+    
+}
