d290ddacec48cfb45c22924ac580dc625fba3977
Prometedor <90892290+syulyul@users.noreply.github.com>
2024-04-16 21:13:55 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+  private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+  public static void main(String[] args) throws Exception {
+    int N = Integer.parseInt(br.readLine());
+    for (int i = 0; i < N; i++) {
+      for (int j = N - i; j > 0; j--) {
+        bw.write("*");
+      }
+      bw.write("\n");
+    }
+    br.close();
+    bw.flush();
+    bw.close();
+  }
+}
