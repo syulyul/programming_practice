ac717aacd031a07b83c72a49d1878047057e619b
Prometedor <90892290+syulyul@users.noreply.github.com>
2024-05-17 16:30:31 +0900

    [Silver I] Title: 경로 찾기, Time: 408 ms, Memory: 19968 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+        int N = Integer.parseInt(br.readLine());
+        int[][] graph = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 플로이드-와샬 알고리즘
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (graph[i][k] == 1 && graph[k][j] == 1)
+                        graph[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(graph[i][j] + " ");
+            }
+            System.out.println();
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
